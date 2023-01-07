badc6278f20583a7dbd1c5afc64789a49132290c
Gleb <gleb.verbickiy@yandex.ru>
2023-01-08 00:21:46 +0300

    Оставил все как есть

--- src/model/Task.java
+++ src/model/Task.java
@@ -3,17 +3,36 @@ package model;
 import java.util.Objects;
 
 public class Task {
-    protected String title;
-
-    protected String description;
     protected int id;
-    protected String status;
+    protected String title;
+    protected String description;
+    protected StatusEnum status;
 
-    public Task(String title, String description, String status) {
+    public Task(int id, String title, String description, StatusEnum status) {
+        this.id = id;
         this.title = title;
         this.description = description;
-        this.id = 0;
         this.status = status;
+    }
+
+    public Task(String title, String description, StatusEnum status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task)) return false;
+        Task задача = (Task) o;
+        return id == задача.id && Objects.equals(title, задача.title) &&
+                Objects.equals(description, задача.description) && Objects.equals(status, задача.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, id, status);
     }
 
     public String getTitle() {
@@ -24,59 +43,38 @@ public class Task {
         this.title = title;
     }
 
-    public int getId() {
-
-        return id;
-    }
-
-    public void setId(int id) {
-
-        this.id = id;
-    }
-
     public String getDescription() {
-
         return description;
     }
 
     public void setDescription(String description) {
-
         this.description = description;
     }
 
-    public String getStatus() {
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public StatusEnum getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-
+    public void setStatus(StatusEnum status) {
         this.status = status;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return id == task.id && Objects.equals(title, task.title) && Objects.equals(description, task.description) && Objects.equals(status, task.status);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(title, description, id, status);
-    }
-
-    @Override
     public String toString() {
-        return "Task{" +
-                    " title='" + title + '\'' +
-                    ", description='" + description + '\'' +
-                    ", id=" + id +
-                    ", status='" + status + '\'' +
-                    '}';
+        return "Task{ " +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
     }
-
 
 }
