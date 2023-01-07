7d25f48354a417c8cfcbdd8109af2f6fb1153eb5
Gleb <gleb.verbickiy@yandex.ru>
2023-01-08 00:10:35 +0300

    1) Добавлен List<Integer> subtasksId; 2) Добавлены equals и hashCode.

--- src/model/Epic.java
+++ src/model/Epic.java
@@ -1,19 +1,47 @@
 package model;
 
-public class Epic extends Task{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Epic extends Task {
+    private List<Integer> subtasksId;
 
     public Epic(String title, String description) {
-        super(title, description, Status.NEW);
+        super(title, description, StatusEnum.NEW);
+        this.subtasksId = new ArrayList<>();
+    }
+
+    public List<Integer> getSubtasksId() {
+        return subtasksId;
+    }
+
+    public void setSubtasksId(List<Integer> subtasksId) {
+        this.subtasksId = subtasksId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Epic epic = (Epic) o;
+        return Objects.equals(subtasksId, epic.subtasksId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), subtasksId);
     }
 
     @Override
     public String toString() {
-        return "Epic{" +
-                        ", title='" + title + '\'' +
-                        ", description='" + description + '\'' +
-                        ", id=" + id +
-                        ", status='" + status + '\'' +
-                        '}';
+        return "Epic{ " +
+                "id=" + getId() +
+                ", title='" + getTitle() + '\'' +
+                ", description='" + getDescription() + "'" +
+                ", subtasksIds=" + subtasksId +
+                ", status='" + getStatus() + "'" +
+                '}';
     }
 }
-
